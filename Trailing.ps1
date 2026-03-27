cc30725c0bf1e1f853b2546c7b30e79e3d0b4f8b
miroslav.trankov <miroslav.trankov@mentormate.com>
2026-03-27 17:51:09 +0200

    commit

---
+++ Trailing.ps1
@@ -0,0 +1,9 @@
+﻿while ($true) {
+  Clear-Host
+  Get-Date
+  ""
+  Select-String -Path bot_events.csv -Pattern "SETUP_PENDING|SETUP_CONFIRMED|TRADE_OK|TRADE_FAIL|TRAILING_STOP_OK|TRAILING_STOP_FAIL|RISK_CLOSE_OK|RISK_CLOSE_FAIL" |
+    Select-Object -Last 20 |
+    ForEach-Object { $_.Line }
+  Start-Sleep -Seconds 5
+}
